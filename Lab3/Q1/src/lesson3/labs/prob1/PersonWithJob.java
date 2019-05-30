566cf23ae40a3f692354496780e31c88bb224939
Arley Wilches <arley.wilches@gmail.com>
2019-05-29 21:41:00 -0500

    is changed the comparasion method body

--- Lab3/Q1/src/lesson3/labs/prob1/PersonWithJob.java
+++ Lab3/Q1/src/lesson3/labs/prob1/PersonWithJob.java
@@ -33,8 +33,8 @@ public class PersonWithJob {
 		PersonWithJob p1 = new PersonWithJob("Joe", 30000);
 		Person p2 = new Person("Joe");
 		// As PersonsWithJobs, p1 should be equal to p2
-		System.out.println("p1.equals(p2)? " + p1.equals(p2));
-		System.out.println("p2.equals(p1)? " + p2.equals(p1));
+		System.out.println("p1.equals(p2)? " + p1.person.equals(p2));
+		System.out.println("p2.equals(p1)? " + p2.equals(p1.person));
 	}
 
 }
