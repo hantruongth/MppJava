0d392199bbc7e838d1fda9c7fd006034a03db62b
Han Truong <hantruongth@gmail.com>
2019-05-31 21:23:44 -0500

    update and add output

--- Lab5/Q1/src/prob1/extpackage/main.java
+++ Lab5/Q1/src/prob1/extpackage/main.java
@@ -1,17 +1,14 @@
 package prob1.extpackage;
+
 import prob1.*;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Duck[] ducks = {
-			new MallardDuck(),
-			new DecoyDuck(),
-			new RedheadDuck(),
-			new RubberDuck()
-		};
-		for(Duck d: ducks) {
-			System.out.println(d.getClass().getSimpleName()+":");
+		Duck[] ducks = { new MallardDuck(), new DecoyDuck(), new RedheadDuck(), new RubberDuck() };
+		for (Duck d : ducks) {
+			System.out.println(d.getClass().getSimpleName() + ":");
 			System.out.print("	");
 			d.display();
 			System.out.print("	");
