e7295988e79052d6908aa26bbe0351d18884db9b
Han Truong <hantruongth@gmail.com>
2019-06-03 14:03:55 -0500

    remove old project

--- MppProjectGroup16/src/mum/project/group16/constant/Constants.java
+++
@@ -1,9 +0,0 @@
-package mum.project.group16.constant;
-
-public class Constants {
-	public static final String USER_ADMIN = "admin";
-	public static final String USER_LIBRARIAN = "librarian";
-	public static final String DEFAULT_PASSWORD = "123456";
-	
-
-}
