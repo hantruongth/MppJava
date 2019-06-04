e6776cf89008d23fdd58cd7c0ca8cf0a97e17630
Dinh Hieu Ho <dhho@mum.edu>
2019-06-04 16:40:23 -0500

    Hieu, update add member window

--- Group16_MppProject/src/ui/AddMemberWindow.java
+++ Group16_MppProject/src/ui/AddMemberWindow.java
@@ -6,12 +6,9 @@ import java.util.HashMap;
 import java.util.List;
 
 import business.Address;
-import business.Author;
 import business.ControllerInterface;
 import business.LibraryMember;
 import business.SystemController;
-import dataaccess.DataAccess;
-import dataaccess.DataAccessFacade;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -26,6 +23,10 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
@@ -40,6 +41,14 @@ public class AddMemberWindow extends Stage implements LibWindow{
 
 	public static final AddMemberWindow INSTANCE = new AddMemberWindow();
 	private TableView<LibraryMember> tableMemberView = new TableView<LibraryMember>();
+	TextField memberIDTextField;
+	TextField firstNameTextField;
+	TextField lastNameTextField;
+	TextField streetTextField;
+	TextField cityTextField;
+	TextField stateTextField;
+	TextField zipCodeTextField;
+	TextField telePhoneTextField;
 
 	private boolean isInitialized = false;
 	private ControllerInterface c = new SystemController();
@@ -76,57 +85,56 @@ public class AddMemberWindow extends Stage implements LibWindow{
 		Label isbnLabel = new Label("Member ID:");
 		grid.add(isbnLabel, 0, 1);
 
-		TextField memberIDTextField = new NumberTextField();
+		memberIDTextField = new NumberTextField();
 		grid.add(memberIDTextField, 1, row);
 
 		row++;
 		Label firstNameLabel = new Label("First Name:");
 		grid.add(firstNameLabel, 0, row);
 
-		TextField firstNameTextField = new TextField();
+		firstNameTextField = new TextField();
 		grid.add(firstNameTextField, 1, row);
 		
 		row++;
 		Label lastNameLabel = new Label("Last Name:");
 		grid.add(lastNameLabel, 0, row);
 
-		TextField lastNameTextField = new TextField();
+		lastNameTextField = new TextField();
 		grid.add(lastNameTextField, 1, row);
 		
 		row++;
 		Label streetLabel = new Label("Street:");
 		grid.add(streetLabel, 0, row);
 
-		TextField streetTextField = new TextField();
+		streetTextField = new TextField();
 		grid.add(streetTextField, 1, row);
 		
 		row++;
 		Label cityLabel = new Label("City:");
 		grid.add(cityLabel, 0, row);
 
-		TextField cityTextField = new TextField();
+		cityTextField = new TextField();
 		grid.add(cityTextField, 1, row);
 		
 		row++;
 		Label stateLabel = new Label("State:");
 		grid.add(stateLabel, 0, row);
 
-		TextField stateTextField = new TextField();
+		stateTextField = new TextField();
 		grid.add(stateTextField, 1, row);
 		
 		row++;//7
 		Label zipCodeLabel = new Label("Zip Code:");
 		grid.add(zipCodeLabel, 0, row);
 
-		TextField zipCodeTextField = new NumberTextField();
-		
+		zipCodeTextField = new NumberTextField();		
 		grid.add(zipCodeTextField, 1, row);
 		
 		row++;//8
 		Label telephoneLabel = new Label("Telephone:");
 		grid.add(telephoneLabel, 0, row);
 
-		TextField telePhoneTextField = new NumberTextField();
+		telePhoneTextField = new NumberTextField();
 		grid.add(telePhoneTextField, 1, row);
 		
 		Button saveBtn = new Button("Save");
@@ -178,10 +186,24 @@ public class AddMemberWindow extends Stage implements LibWindow{
 		row++;//10
 		grid.add(tableMemberView, 1, row);
 		
+		tableMemberView.setOnMouseClicked((MouseEvent event) -> {
+	        if(event.getButton().equals(MouseButton.PRIMARY)){
+	        	doSelectMember();
+	        }
+	    });
+		tableMemberView.setOnKeyReleased(new EventHandler<KeyEvent>() {
+	        @Override
+	        public void handle(KeyEvent ke)
+	        {
+	            if (ke.getCode().equals(KeyCode.UP) || ke.getCode().equals(KeyCode.DOWN)) {
+		            doSelectMember();
+	            }
+	        }
+	    });
 		this.bindMemberToList(getLibraryMember());
 		
 		Screen screen = Screen.getPrimary();
-		Rectangle2D bounds = screen.getVisualBounds();
+		//Rectangle2D bounds = screen.getVisualBounds();
 
 		Scene scene = new Scene(grid/*, bounds.getWidth()/1.5, bounds.getHeight()/2*/);
         setScene(scene);
@@ -198,5 +220,16 @@ public class AddMemberWindow extends Stage implements LibWindow{
 	}
 	private void addNewMember(LibraryMember newMember) {
 		c.addLibraryMember(newMember);
+	}
+	private void doSelectMember() {
+		LibraryMember selMember = tableMemberView.getSelectionModel().getSelectedItem();
+        memberIDTextField.setText(selMember.getMemberId());
+        firstNameTextField.setText(selMember.getFirstName());
+        lastNameTextField.setText(selMember.getLastName());
+        streetTextField.setText(selMember.getAddress().getStreet());
+        cityTextField.setText(selMember.getAddress().getCity());
+        stateTextField.setText(selMember.getAddress().getState());
+        zipCodeTextField.setText(selMember.getAddress().getZip());
+        telePhoneTextField.setText(selMember.getTelephone());
 	}
 }
