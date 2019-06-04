168ffe30437568cdb37dbdcd728bebe84f4529a5
Arley Wilches <arley.wilches@gmail.com>
2019-06-04 11:20:19 -0500

    i push make a copy

--- Group16_MppProject/src/dataaccess/DataAccess.java
+++ Group16_MppProject/src/dataaccess/DataAccess.java
@@ -10,5 +10,6 @@ public interface DataAccess {
 	public HashMap<String,Book> readBooksMap();
 	public HashMap<String,User> readUserMap();
 	public HashMap<String, LibraryMember> readMemberMap();
-	public void saveNewMember(LibraryMember member); 
+	public void saveNewMember(LibraryMember member);
+	public void saveBook(Book book);
 }
