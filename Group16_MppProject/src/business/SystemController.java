1eb85a30ccfdc70ba8f6ecc89bfdcc7cc2bee32e
Han Truong <hantruongth@gmail.com>
2019-06-06 11:25:40 -0500

    refactor code

--- Group16_MppProject/src/business/SystemController.java
+++ Group16_MppProject/src/business/SystemController.java
@@ -15,20 +15,21 @@ import dataaccess.User;
 
 public class SystemController implements ControllerInterface {
 	public static Auth currentAuth = null;
-	
+
 	public void login(String id, String password) throws LoginException {
 		DataAccess da = new DataAccessFacade();
 		HashMap<String, User> map = da.readUserMap();
-		if(!map.containsKey(id)) {
+		if (!map.containsKey(id)) {
 			throw new LoginException("ID " + id + " not found");
 		}
 		String passwordFound = map.get(id).getPassword();
-		if(!passwordFound.equals(password)) {
+		if (!passwordFound.equals(password)) {
 			throw new LoginException("Password incorrect");
 		}
 		currentAuth = map.get(id).getAuthorization();
-		
+
 	}
+
 	@Override
 	public List<String> allMemberIds() {
 		DataAccess da = new DataAccessFacade();
@@ -36,7 +37,7 @@ public class SystemController implements ControllerInterface {
 		retval.addAll(da.readMemberMap().keySet());
 		return retval;
 	}
-	
+
 	@Override
 	public List<String> allBookIds() {
 		DataAccess da = new DataAccessFacade();
@@ -45,28 +46,26 @@ public class SystemController implements ControllerInterface {
 		return retval;
 	}
 
-	
 	@Override
-	public List<Book> allBooks(){
+	public List<Book> allBooks() {
 		DataAccess da = new DataAccessFacade();
-		Collection<Book> books = da.readBooksMap().values();	
+		Collection<Book> books = da.readBooksMap().values();
 		List<Book> bs = new ArrayList<>();
 		bs.addAll(books);
 		return bs;
 	}
-	
+
 	@Override
-	public List<LibraryMember> allMemebers(){
+	public List<LibraryMember> allMemebers() {
 		DataAccess da = new DataAccessFacade();
-		Collection<LibraryMember> members = da.readMemberMap().values();	
+		Collection<LibraryMember> members = da.readMemberMap().values();
 		List<LibraryMember> membersList = new ArrayList<>();
 		membersList.addAll(members);
 		return membersList;
 	}
-	
-	
+
 	@Override
-	public CheckoutRecord saveCheckoutRecord(CheckoutRecord record){
+	public CheckoutRecord saveCheckoutRecord(CheckoutRecord record) {
 		DataAccess da = new DataAccessFacade();
 		da.saveNewCheckoutRecord(record);
 		return record;
@@ -78,16 +77,19 @@ public class SystemController implements ControllerInterface {
 		HashMap<String, Author> authorHashMap = da.readAuthorMap();
 		return new ArrayList<>(authorHashMap.values());
 	}
+
 	@Override
 	public void addBook(Book book) {
-		DataAccess da =  new DataAccessFacade();
+		DataAccess da = new DataAccessFacade();
 		da.saveBook(book);
 	}
+
 	@Override
 	public void addLibraryMember(LibraryMember member) {
-		DataAccess da =  new DataAccessFacade();
+		DataAccess da = new DataAccessFacade();
 		da.saveNewMember(member);
 	}
+
 	@Override
 	public List<LibraryMember> allLibraryMembers() {
 		// TODO Auto-generated method stub
@@ -95,14 +97,14 @@ public class SystemController implements ControllerInterface {
 		HashMap<String, LibraryMember> memberHashMap = da.readMemberMap();
 		return new ArrayList<>(memberHashMap.values());
 	}
-	
+
 	@Override
-	public CheckoutEntry saveCheckoutEntry(CheckoutEntry entry){
+	public CheckoutEntry saveCheckoutEntry(CheckoutEntry entry) {
 		DataAccess da = new DataAccessFacade();
 		da.saveNewCheckoutEntry(entry);
 		return entry;
 	}
-	
+
 	public List<LibraryMember> getLibraryMember(String memberId) {
 		DataAccess da = new DataAccessFacade();
 		HashMap<String, LibraryMember> memberMap = da.readMemberMap();
@@ -113,70 +115,70 @@ public class SystemController implements ControllerInterface {
 			return Arrays.asList(member);
 		}
 		return new ArrayList<>(memberMap.values());
-		
+
 	}
+
 	@Override
 	public List<LibraryMember> getAllLibraryMember() {
 		DataAccess da = new DataAccessFacade();
 		HashMap<String, LibraryMember> memberMap = da.readMemberMap();
 		return new ArrayList<>(memberMap.values());
 	}
-	
+
 	@Override
 	public Map<LocalDate, List<CheckoutEntry>> getCheckoutEntries(LibraryMember member) {
 		DataAccess da = new DataAccessFacade();
 		HashMap<String, CheckoutRecord> checkoutHashMap = da.readCheckoutRecordMap();
 		Collection<CheckoutRecord> checkoutRecords = checkoutHashMap.values();
 		List<CheckoutRecord> checkoutRecordOfMember = new ArrayList<>();
-		checkoutRecords.forEach(e->{
-			if(e.getMember().equals(member))
+		checkoutRecords.forEach(e -> {
+			if (e.getMember().equals(member))
 				checkoutRecordOfMember.add(e);
 		});
 		Map<LocalDate, List<CheckoutEntry>> checkoutEntriesMap = new HashMap<>();
-		checkoutRecordOfMember.forEach(e->{
+		checkoutRecordOfMember.forEach(e -> {
 			List<CheckoutEntry> existList = checkoutEntriesMap.get(e.getDate());
-			if(existList == null)
+			if (existList == null)
 				existList = new ArrayList<>();
-			
+
 			existList.addAll(e.getEntries());
 			checkoutEntriesMap.put(e.getDate(), existList);
-			
+
 		});
 		return checkoutEntriesMap;
 	}
+
 	@Override
 	public Map<LibraryMember, List<CheckoutEntry>> getCheckoutEntryList(String isbn) {
 		DataAccess da = new DataAccessFacade();
 		HashMap<String, CheckoutRecord> checkoutHashMap = da.readCheckoutRecordMap();
 		Collection<CheckoutRecord> checkoutRecords = checkoutHashMap.values();
 		Map<LibraryMember, List<CheckoutEntry>> memberEntryMap = new HashMap<>();
-		checkoutRecords.forEach(e-> {
+		checkoutRecords.forEach(e -> {
 			List<CheckoutEntry> overdueEntries = new ArrayList<>();
-			
+
 			List<CheckoutEntry> entries = e.getEntries();
-			
+
 			for (CheckoutEntry entry : entries) {
-				if(isbn == null && entry.getDueDate().isBefore(LocalDate.now())) {
+				if (isbn == null && entry.getDueDate().isBefore(LocalDate.now())) {
 					overdueEntries.add(entry);
-				}
-				else if(isbn != null && entry.getCopy().getBook().getIsbn().equalsIgnoreCase(isbn) && entry.getDueDate().isBefore(LocalDate.now())){
+				} else if (isbn != null && entry.getCopy().getBook().getIsbn().equalsIgnoreCase(isbn)
+						&& entry.getDueDate().isBefore(LocalDate.now())) {
 					overdueEntries.add(entry);
 				}
 			}
-			
-			if(memberEntryMap.get(e.getMember()) != null) {
+
+			if (memberEntryMap.get(e.getMember()) != null) {
 				overdueEntries.addAll(memberEntryMap.get(e.getMember()));
 				memberEntryMap.put(e.getMember(), overdueEntries);
-			}else {
-				if(overdueEntries.size() > 0)
+			} else {
+				if (overdueEntries.size() > 0)
 					memberEntryMap.put(e.getMember(), overdueEntries);
 			}
-			
+
 		});
-		
+
 		return memberEntryMap;
 	}
-	
-	 
-	
+
 }
